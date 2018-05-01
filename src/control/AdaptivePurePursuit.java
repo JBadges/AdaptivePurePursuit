891392df7634549677b5b6aec6402fb6eff31601
JBadges <jackson.brajer@gmail.com>
2018-05-01 16:45:39 -0400

    Goal Point From Robot pose and lookahead

--- src/control/AdaptivePurePursuit.java
+++ src/control/AdaptivePurePursuit.java
@@ -1,11 +1,21 @@
 package control;
 
+import util.Point3;
+import util.SpeedPoint;
+
 public class AdaptivePurePursuit {
 
-    Path path;
+    private Path path;
+    private double lookahead;
 
-    public AdaptivePurePursuit (Path path) {
+    public AdaptivePurePursuit (Path path, double lookahead) {
         this.path = path;
+        this.lookahead = lookahead;
+    }
+
+    public SpeedPoint update(Point3 pose) {
+        Point3 goalPoint = path.getGoalPoint(pose, lookahead);
+        return new SpeedPoint(0, 0);
     }
     
 }

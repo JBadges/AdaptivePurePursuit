a7aa9ccf4cff7731c06ee1fcae17b2c80c6982b5
JBadges <jackson.brajer@gmail.com>
2018-05-02 10:43:37 -0400

    Main Menu

---
+++ src/gui/MainMenu.java
@@ -0,0 +1,28 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainMenu extends GUIScene {
+
+    public static Scene getScene() {
+        StackPane sp = new StackPane();
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+        sp.getChildren().add(btn);
+        Scene scene = new Scene(sp, 500,500);
+        return scene;
+    }
+    
+}

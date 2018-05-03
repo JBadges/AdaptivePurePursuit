ae15eaa64bdb4f7b1a52d0ffc0f6c6096abd3d5c
JBadges <jackson.brajer@gmail.com>
2018-05-03 11:37:40 -0400

    Animation Loop and Robot Position
    Physics calculations needed

--- src/gui/SimulationScene.java
+++ src/gui/SimulationScene.java
@@ -1,5 +1,7 @@
 package gui;
 
+import control.PathSegment;
+import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -8,10 +10,16 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 import util.Point3;
 
 public class SimulationScene {
+
+    //Time seconds
+    private static double currentTime = 0.0;
+    private static double lastUpdate;
 
     public static Scene getScene() {
         Pane sp = new Pane();
@@ -50,9 +58,79 @@ public class SimulationScene {
                     txtf_robotMassKg.setText("Must be numeric");
                     isValidInput = false;
                 }
+                
                 //Input is valid - Commence robot simulation
                 if (isValidInput) {
+                    final Rectangle robotRect = new Rectangle(-100, -100, 25, 25);
+                    class SkidDriveRobot {
+                        public Point3 position;
+                        //In m/s
+                        public double leftVelocity;
+                        public double rightVelocity;
 
+                        //In Kg
+                        public double mass;
+                        //In m
+                        public double wheelDistance;
+
+                        public void updateLeftAcceleration(double voltage) {
+                            
+                        }
+
+                        public void updateRightAcceleration(double voltage) {
+                            
+                        }
+
+                        public void updatePos(double dt) {
+                            
+                        }
+                    }
+
+                    //Robot information setup
+                    SkidDriveRobot robot = new SkidDriveRobot();
+                    robot.wheelDistance = Double.parseDouble(txtf_wheelDist.getText());
+                    robot.mass = Double.parseDouble(txtf_robotMassKg.getText());
+                    Point3 starting = PathCreation.getWaypoints().get(0);
+                    Point3 second = PathCreation.getWaypoints().get(1);
+                    PathSegment startToSecond = new PathSegment(starting, second);
+                    robot.position = new Point3(starting.getX(), starting.getY(), Math.atan(startToSecond.getSlope()));
+
+                    AnimationTimer loop = new AnimationTimer(){
+                        @Override
+                        public void handle(long now) {
+                            if(currentTime > 15) {
+                                this.stop();
+                            }
+                            final double dt = (now-lastUpdate) / 1000000000.0;
+                            //TODO: get voltage for left and right sides based on robot position and goal position
+                            double voltageLeft = 0;
+                            double voltageRight = 0;
+
+                            robot.updateLeftAcceleration(voltageLeft);
+                            robot.updateRightAcceleration(voltageRight);
+
+                            robot.updatePos(dt);
+                            currentTime += dt;
+
+                            robotRect.setX(robot.position.getX()-25/2.0);
+                            robotRect.setY(robot.position.getY()-25/2.0);
+                            
+                            robotRect.setFill(new Color(0, 1, 0, 0.3));
+                            robotRect.setRotate(Math.toDegrees(robot.position.getTheta()));
+
+                            try {
+								Thread.sleep(5);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+                            }
+                            lastUpdate = now;
+                        }
+                    };
+
+                    lastUpdate = System.nanoTime();
+                    loop.start();
+
+                    sp.getChildren().add(robotRect);
                 }
             }
         });
